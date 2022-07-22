582f2692b99681563b9e536a1fc84130332dd78f
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2022-07-21 21:59:44 -0500

    avances v2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,9 +5,9 @@
     .titulo-principal.color-acento-contenido.mb-0
       .titulo-principal__numero
         span.text-white 2
-      h1 Marketing
+      h1 #[em marketing]
     .bg_grad_05
-      p.mb-5.pt-5 El marketing digital o marketing online hace referencia a todas aquellas operaciones y estrategias comerciales y publicitarias que se desarrollan en los medios y canales de Internet, tal como se muestra a continuación:
+      p.mb-5.pt-5 El #[em marketing] digital o #[em marketing online] hace referencia a todas aquellas operaciones y estrategias comerciales y publicitarias que se desarrollan en los medios y canales de Internet, tal como se muestra a continuación:
       figure(data-aos="zoom-in")
         .video
           iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
@@ -17,22 +17,139 @@
     #t_2_1.titulo-segundo.color-acento-contenido(data-aos="fade-in")
       h2 2.1  #[em Marketing] relacional
 
-    p.mb-5 #[span(style="background-color: #ECE5F7;") “Es el conjunto de estrategias de construcción y diseminación de marca, prospección, fidelización y creación de autoridad en el mercado. El objetivo del marketing relacional es conquistar y fidelizar a los clientes, además de convertirlos en defensores y promotores de la marca” Lipinski (2020).] Hace referencia a un conjunto de métodos utilizados para construir relaciones estratégicas a largo plazo con los clientes; dicha correlación se fundamenta en el objetivo de retener a los clientes y expandir un negocio. Lo importante de esta relación es que está en capacidad de beneficiar tanto a la empresa como al cliente, con el fin de fortalecerla. 
+    p.mb-5 #[span(style="background-color: #ECE5F7;") “Es el conjunto de estrategias de construcción y diseminación de marca, prospección, fidelización y creación de autoridad en el mercado. El objetivo del #[em marketing] relacional es conquistar y fidelizar a los clientes, además de convertirlos en defensores y promotores de la marca” Lipinski (2020).] Hace referencia a un conjunto de métodos utilizados para construir relaciones estratégicas a largo plazo con los clientes; dicha correlación se fundamenta en el objetivo de retener a los clientes y expandir un negocio. Lo importante de esta relación es que está en capacidad de beneficiar tanto a la empresa como al cliente, con el fin de fortalecerla. 
 
     .d-flex.flex-wrap.mb-5(style="background-color: #DEF2F4")
       img.col-lg-4.mb-4.mb-lg-0(src="@/assets/curso/temas/t2/t2_01.png")
       .col-lg-7.ps-lg-4.d-flex.flex-wrap.align-content-center
-        p Se podrá brindar un valor agregado con el cual se genere familiaridad entre los consumidores, el marketing relacional no se enfoca en el producto, por el contrario, prioriza por encima de todo al cliente, porque no se trata solo de cerrar una venta, sino que va más allá de eso, siendo esta una forma de atraer con facilidad clientes a una empresa. 
+        p Se podrá brindar un valor agregado con el cual se genere familiaridad entre los consumidores, el #[em marketing] relacional no se enfoca en el producto, por el contrario, prioriza por encima de todo al cliente, porque no se trata solo de cerrar una venta, sino que va más allá de eso, siendo esta una forma de atraer con facilidad clientes a una empresa. 
         p.mb-0 El objetivo principal del #[em marketing] relacional es alcanzar la lealtad del cliente a través de que él se sienta satisfecho en todo sentido, debido a que contar con un cliente fiel será determinante para optimizar los resultados de una empresa, de igual forma conocer muy bien a sus clientes le permitirá conocer información importante con la cual pueda estructurar las diversas estrategias de #[em marketing] para la empresa.
 
-    p A continuación se enuncian algunas ventajas del #[em(style="background-color: #ECE5F7") marketing] relacional, las cuales podrá poner práctica al momento de buscar, enamorar y potencializar clientes:
+    p.mb-5 A continuación se enuncian algunas ventajas del #[em(style="background-color: #ECE5F7") marketing] relacional, las cuales podrá poner práctica al momento de buscar, enamorar y potencializar clientes:
+
+    .container-bg-t2
+      img.img-bg(src="@/assets/curso/temas/t2/bg_01.svg")
+      SlyderF.mb-5(columnas="col-lg-6")
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-01.svg")
+          p #[span(style="color: #43A99B") ●] Comienza a partir del primer contacto de un visitante a una empresa, en este punto haga lo necesario para no perder la interacción con él, este proceso puede ser simple, si es por intermedio de una red social un (me gusta) es el mejor comienzo, ya tendrá referencia de su marca y esto lo hace importante para que se convierta en un visitante y cliente potencial (fidelizar clientes).
+        
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-02.svg")
+          p #[span(style="color: #43A99B") ●] Si desea aumentar sus ventas es vital que conozca las necesidades y expectativas de los clientes por intermedio de una buena relación, donde prime la empatía, de esta forma sabrá qué acciones tomar con el fin de satisfacerlos, cómo persuadirlos y qué situaciones prefieren a través de las experiencias que tengan con productos o servicios.
+        
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-03.svg")
+          p #[span(style="color: #43A99B") ●] De acuerdo con la relación comercial con los clientes podrá identificar fácilmente qué clientes son recurrentes y cuáles son ocasionales, y qué acciones realizar para que estos últimos se conviertan en recurrentes ampliando su portafolio de clientes.
+        
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-04.svg")
+          p #[span(style="color: #43A99B") ●] De acuerdo con la satisfacción que un cliente tenga por un producto y a la relación comercial que tenga con él, este fácilmente hará un voz a voz de su producto o servicio, siendo una fuente confiable para obtener nuevos clientes.
+
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-05.svg")
+          p #[span(style="color: #43A99B") ●] Permítase tener una retroalimentación constante, esto le ayudará a identificar escenarios de mejora en servicios y productos, que aporten un valor comercial y de relaciones para la empresa.
+          
+        .tarjeta.color-acento-botones.p-4.px-xl-5.h-100(style="background-color: #ECE5F7; border-radius: 0;")
+          img.col-3.col-xl-2.mb-4.mx-auto(src="@/assets/curso/temas/t2/img-06.svg")
+          p #[span(style="color: #43A99B") ●] Tenga precaución al momento de crear una estrategia de interacción con los clientes, en muchas ocasiones una mala intervención generará como resultado una estrategia invasiva, desgastante y sin un resultado positivo.
+
+      .title-four-level.mb-5
+        img.me-3(src="@/assets/curso/temas/t2/d_1.svg" style="max-width: 50px")
+        h4.me-auto #[span Elementos clave del marketing relacional]
+
+      p.mb-5 Ahora, surge una gran pregunta dentro de los elementos básicos de #[em(style="background-color: #ECE5F7") marketing], ¿cómo retener a un cliente durante un determinado tiempo posible? Para dar respuesta tenga presente estos tres elementos:
+
+    .d-flex.flex-wrap.mb-5
+      .col-md-6.col-xl-4.mb-4.mb-xl-0.px-2.mb-2
+        .tarjeta.tarjeta-slide.arriba(style="border-radius: 0; background-color: #FDF3E1;" @mouseover="indicadorTarjetaSlide = false")
+          .indicador--hover(v-if="indicadorTarjetaSlide")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__contenido.p-4
+              h5 Servicio al cliente
+              p La atención al público es un factor diferencial dentro de una estrategia de #[em marketing]. Tenga presente que después que un cliente realicé una compra no lo suelte, es decir, no permita que este se sienta solo, por el contrario, ofrézcale un buen servicio de calidad postventa.
+            .tarjeta-slide__img(style="background-position-y: bottom; background-position-x: center;" :style="{'background-image': `url(${require('@/assets/curso/temas/t2/t2_02.svg')})`}")
+      
+      .col-md-6.col-xl-4.mb-4.mb-xl-0.px-2.mb-2
+        .tarjeta.tarjeta-slide.arriba(style="border-radius: 0; background-color: #FDF3E1;")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__contenido.p-4
+              h5 Calidad del servicio o producto
+              p Independientemente del servicio al cliente ofrezca un producto o servicio de calidad, de lo contrario no le será fácil retener un cliente y poder consolidar una relación comercial a largo plazo.
+            .tarjeta-slide__img(style="background-position-y: bottom; background-position-x: center;" :style="{'background-image': `url(${require('@/assets/curso/temas/t2/t2_03.svg')})`}")
+      
+      .col-md-6.col-xl-4.mb-4.mb-xl-0.px-2.mb-2
+        .tarjeta.tarjeta-slide.arriba(style="border-radius: 0; background-color: #FDF3E1;")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__contenido.p-4
+              h5 #[em Marketing]
+              p El #[em marketing] en redes sociales, de contenido o email, son estrategias básicas y muy fuertes en el ámbito de la mercadotecnia, tómelas como base y mantenga las relaciones comerciales con sus clientes.
+            .tarjeta-slide__img(style="background-position-y: bottom; background-position-x: center;" :style="{'background-image': `url(${require('@/assets/curso/temas/t2/t2_04.svg')})`}")
+
+    p.mb-5 El #[em Marketing] relacional no es una estrategia que solo se da a corto plazo, por el contrario, su finalidad es construir relaciones duraderas, preferentemente progresivas, intentando que el cliente se convierta en fan, así que haga lo posible para obtener este resultado.
+
+    Separador
+
+    #t_2_2.titulo-segundo.color-acento-contenido(data-aos="fade-in")
+      h2 2.2  Estrategias de #[em Marketing]
+
+    p Una estrategia de #[em Marketing] es la organización de tareas específicas para alcanzar los objetivos trazados en los medios online. Esto incluye crear contenido y entregarlo a través de las redes sociales y sitios web, administración de blogs, correos electrónicos, entre otros. Este tipo de #[em Marketing] se ha vuelto fundamental, no solo para las empresas que operan comercio electrónico o venden solo a través de Internet, de hecho esta es una estrategia esencial para todas las empresas, porque si no utilizan este tipo de herramientas podrán perder algunos de sus clientes potenciales.
+    p.mb-5 Las siguientes son las cinco estrategias fundamentales en #[em Marketing] y las más utilizadas por los expertos en el tema:
+
+    AcordionA.col-xl-10.mx-auto(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")
+      .row.align-items-center(titulo="(SEO) Posicionamiento en buscadores")
+        .col-lg-5.mb-5.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/t2/t2_05.png" style="max-width: 353px")
+        .col-lg-7
+          p.mb-0 Hace referencia al Search Engine #[em Marketing], se asocia a las acciones del #[em Marketing] en los buscadores y por lo general se utiliza en anuncios de campañas. Estos son anuncios de motores de búsqueda patrocinados y a diferencia del SEO, son estrategias pagas. Cuenta con una gran ventaja y es que sus buscadores son Google Adwords, Bing Ads, Yahoo, produciendo tráfico de alta calidad en la Web para permitir una buena segmentación del mercado.
+
+      .row.align-items-center(titulo="(SEM) Campañas de anuncios en buscadores")
+        .col-lg-5.mb-5.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/t2/t2_06.png" style="max-width: 353px")
+        .col-lg-7
+          p.mb-0 Hace referencia al Search Engine Marketing, se asocia a las acciones del marketing en los buscadores y por lo general se utiliza en anuncios de campañas. Estos son anuncios de motores de búsqueda patrocinados y a diferencia del SEO, son estrategias pagas. Cuenta con una gran ventaja y es que sus buscadores son Google Adwords, Bing Ads, Yahoo, produciendo tráfico de alta calidad en la Web para permitir una buena segmentación del mercado.
+      
+      .row.align-items-center(titulo="Blogs")
+        .col-lg-5.mb-5.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/t2/t2_07.png" style="max-width: 353px")
+        .col-lg-7
+          p.mb-0 El diseño y la creación de un contenido en un blog empresarial es el complemento adecuado en una página web, es la plataforma de interacción para que las empresas conozcan los productos y novedades del sector al cual está dirigido su nicho de mercado, de igual forma, atraerá visitantes a la Web corporativa, siendo una estrategia de #[em Marketing] fundamental.
+      
+      .row.align-items-center(titulo="Email Marketing")
+        .col-lg-5.mb-5.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/t2/t2_08.png" style="max-width: 353px")
+        .col-lg-7
+          p.mb-0 Es una estrategia para atraer clientes potenciales por intermedio de correos electrónicos. Esta es una técnica que no logra atrapar la mayor cantidad de clientes, es decir, es poco invasiva, debido a que los usuarios dan un consentimiento previo para recibir cualquier tipo de información de la empresa. Es el lugar perfecto para fidelizar clientes, ofreciendo contenidos exclusivos como novedades, promociones y ofertas especiales.
+
+      .row.align-items-center(titulo="Redes sociales")
+        p El principal objetivo del uso de las redes sociales en una empresa no se enfoca en vender, por el contrario, su objetivo es crear un cierto vínculo entre el cliente y la fidelidad de una marca. Cada empresa determina cuál red social se adapta mejor al negocio, siendo las más comunes:
+        .col-xl-11.mx-auto.mb-4
+          img(src="@/assets/curso/temas/t2/t2_09.png")
+          .p-4(style="background-color: white")
+            .d-flex.mb-3
+              img.me-3.mb-auto(src="@/assets/curso/temas/t2/redes_1.svg" style="max-width: 58px;")
+              p.mb-0 #[b Facebook:] es el gigante de las redes, la gran mayoría de las empresas cuentan con sus servicios, su estrategia principal es la creación de perfiles que pueden ser de carácter personal o empresarial, allí pueden agrupar un sinnúmero de seguidores, los cuales de forma instantánea recibirán las novedades de sus productos o servicios.
+
+            .d-flex.mb-3
+              img.me-3.mb-auto(src="@/assets/curso/temas/t2/redes_2.svg" style="max-width: 58px;")
+              p.mb-0 #[b Twitter:] su gran ventaja es la inmediatez y la forma rápida de dar respuesta a una solitud en el marco empresarial.
+            
+            .d-flex.mb-3
+              img.me-3.mb-auto(src="@/assets/curso/temas/t2/redes_3.svg" style="max-width: 58px;")
+              p.mb-0 #[b Instagram:] actualmente la mayoría de los mercados se orientan al sector juvenil como estrategia principal de ventas.
+            
+            .d-flex.mb-3
+              img.me-3.mb-auto(src="@/assets/curso/temas/t2/redes_4.svg" style="max-width: 58px;")
+              p.mb-0 #[b LinkedIn:] es sin duda alguna la red profesional de mayor audiencia, se utiliza bastante en recursos humanos y búsqueda activa de trabajo en diferentes ámbito.
+        p El mundo del #[em(style="background-color: #ECE5F7") marketing] es bastante amplio, puede tornarse abrumador, por eso lo más importante es elegir por dónde comenzar y determinar cuál es el mayor beneficio para una empresa con esta estrategia y poder activar una campaña adecuada. 
+
 </template>
 
 <script>
 export default {
   name: 'Tema2',
   data: () => ({
-    // variables de vue
+    indicadorTarjetaSlide: true,
   }),
   mounted() {
     this.$nextTick(() => {
@@ -45,4 +162,30 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.container-bg-t2
+  .img-bg
+    margin-top: auto
+    margin-bottom: auto
+    position: absolute
+    left: 0
+    width: calc(100% + 6rem)
+  .slyder-f
+    .fa-chevron-right, .fa-chevron-left:before
+      color: #EEAC3A
+  .title-four-level
+    display: flex
+    align-items: flex-end
+    position: relative
+    h4
+      margin-bottom: 0
+      position: relative
+      span::after
+        content: ''
+        position: absolute
+        bottom: -3px
+        left: 0
+        width: 100%
+        height: 2px
+        background-color: #43A99B
+</style>
