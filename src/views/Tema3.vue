8275000d7e1f351e1be4182e580ab76b033b8e33
Francisco Lizcano <flizcanor@icloud.com>
2022-07-29 00:40:03 -0500

    carga de contenido

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -4,9 +4,133 @@
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
-        span 3
-      h1 Titulo de primer nivel
+       span.text-white 3
+      h1 Postventa digital
 
+    p.mb-4 Es un servicio que se ofrece a un comprador posterior a la venta de un producto determinado, dicho servicio se podrá brindar por días, meses e incluso años, hace referencia a un conjunto de actividades realizadas después llevar a cabo una venta. Por lo general, son el resultado de una garantía asociada con el producto o servicio que compró. Se enfoca en varias actividades como asistencia, asesoramiento técnico, resolución de problemas y entrega de repuestos (si es el caso).
+
+    .p-4(style="background-color: #DEF2F4").mb-4
+      .row.align-items-center
+
+        .col-lg-7
+          p.mb-4 Un servicio destinado a brindar asistencia con cualquier problema que pueda ocurrir con un producto después de que el comprador empieza a darle vida útil al producto. De esta manera, tiene garantizado el soporte del fabricante en caso de que se requieran reparaciones, devoluciones o reemplazos. Tenga en cuenta que para realizar un adecuado servicio postventa debe seguir una serie de requerimientos fundamentales, con el fin de fidelizar al 100% los clientes.
+
+          p La búsqueda de clientes es una tarea diaria de la gran mayoría de empresas y para ello crear una cultura de lealtad es esencial para la búsqueda del éxito. Los clientes al momento de hacer una compra tienen una buena experiencia de acuerdo con las características que el cliente busca, aquí comienza la relación del producto con el cliente generando un escenario de satisfacción, dando paso a que conozcan el producto, lo que podrá generar nuevos clientes, posterior a esto se da paso al proceso de postventa. Al aprender más sobre el concepto y comprender la importancia de la postventa en las relaciones con los clientes estará listo para pensar y ajustar la estrategia de ser el caso.
+
+        .col-lg-5
+          img.mb-4.mb-lg-0(src="@/assets/curso/temas/t3/t3_01.png")
+
+    p.mb-4 Al momento de diseñar una estrategia de servicio postventa no se limite simplemente a dar respuesta oportuna cada que un cliente lo contacte, fortalezca dicho escenario porque el cliente es parte fundamental de su empresa.
+
+    .row.mb-4
+      .col-10
+        .p-4(style="background-color:#ECE5F7")
+          p Tenga en cuenta los siguientes pasos, los cuales lo apoyarán al momento de diseñar una estrategia de proceso de postventa:
+
+    .bg_img_04
+      .row.mb-4.align-items-center
+        .col-lg-3.d-none.d-lg-block
+          img.mb-4.mb-lg-0(src="@/assets/curso/temas/t3/t3_02.png")
+        .col-lg-9
+
+          TabsA.color-acento-botones
+            .tarjeta.color-acento-botones--borde.p-4(titulo="PASO 1")
+              h4 Seguimiento personalizado
+
+              p.mb-4 Uno de los beneficios de un servicio postventa eficiente es que puede conectarse con sus clientes, comprender si su compra fue satisfactoria, comprender los pros y los contras del proceso y obtener comentarios para identificar puntos de mejora. El seguimiento ayuda tanto a analizar el escenario como a recordarle al cliente que la empresa se preocupa por la satisfacción y las necesidades del cliente. Una manera fácil de mantenerse en contacto con sus clientes es usar el correo electrónico. Al obtener contenido realmente relevante, estos sin duda alguna serán leales al producto.
+
+            .tarjeta.color-acento-botones--borde.p-4(titulo="PASO 2")
+              h4 Diseñe un programa de fidelización
+
+              p.mb-4 Los programas que se enfocan en fidelizar clientes son estrategias que buscan acercarse de manera constante a los clientes, con el fin de brindarles recompensas y beneficios, con experiencias que pueden ser personalizadas y relevantes con un producto, el programa de fidelización le brinda la posibilidad de conocer datos más aproximados sobre sus clientes que puede usar para personalizar y ajustar sus recomendaciones.
+
+            .tarjeta.color-acento-botones--borde.p-4(titulo="PASO 3")
+              h4 Promover lanzamientos
+
+              p.mb-4 Mantenga informados a los clientes sobre lanzamientos, con los datos que recolecte del paso número dos, envié información acerca de los lanzamientos de nuevos productos, ya que esto le puede generar un tráfico constante de visitantes a sus sitios web y por ende, un alto porcentaje de compra de acuerdo con sus productos, variedades y promociones.
+
+            .tarjeta.color-acento-botones--borde.p-4(titulo="PASO 4")
+              h4 Importancia al cliente
+
+              p.mb-4 Una estrategia ideal es la de enviar regalos en fechas especiales, por ejemplo, un cumpleaños, esto hará que la experiencia del cliente hacia el producto crezca de maneta notable, se sentirá parte importante de la emprensa.
+
+            .tarjeta.color-acento-botones--borde.p-4(titulo="PASO 5")
+              h4 Implemente una plataforma de CRM #[em  (Customer Relationship Management)]
+
+              p.mb-4 Esto le permitirá integrar la gran mayoría de actividades postventa, ya que es un soporte multicanal, puesto que tener toda la información de los clientes en un solo espacio hará que la referencia a cada uno de los clientes mejora la experiencia de interacción en el servicio postventa.
+
+
+      p.mb-4 Escuche activamente a sus clientes, es decir, contáctelos para que sean ellos los que le cuenten de la experiencia referente a la venta y con el producto, si percibe alguna anomalía corríjalo de manera rápida, no ignore ningún tipo de sugerencia, recuerde que toda oportunidad es opción de mejora, por último, evalúe la actividad y tome acción de acuerdo con el resultado.
+
+      p.mb-4 Recuerde:
+
+      figure.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t3/t3_03.svg', alt='Recomendaciones de ventas en línea')
+
+      figure.d-block.d-lg-none
+        img(src='@/assets/curso/temas/t3/t3_03_movil.svg', alt='Recomendaciones de ventas en línea')
+
+      p.mb-4 Dentro del contexto de la postventa es importante tener claro la manera cómo se debería llevar a cabo, en este aparte se hablará sobre cuál sería el procedimiento a tener en cuenta al momento de realizar un servicio adecuado, con el fin de seguir fidelizando y captando clientes, recuerde que no solo se trata de hacer una venta y soltar al cliente, con ello buscando tener una buena base de datos de clientes frecuentes y potenciales. Dentro del ejercicio planteado se podrían plantear las siguientes tareas en el procedimiento de la postventa:
+
+    .row.justify-content-center(data-aos="zoom-in").mb-4
+      .col-lg-10
+        .slyderB_bg_01.px-5.py-3.mb-4
+          SlyderA(tipo="b")
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-lg-7.mb-4.mb-lg-0.pt-5.pt-0.position-relative
+                .number(data-number="01")
+
+                  p.ps-4.mb-0 Brinde un soporte técnico de calidad online en tiempo real, no lo vea como un requisito a cumplir, por el contrario, asúmalo como una obligación de la empresa, si los productos que comercializa tienen un alto grado de complejidad, (haciendo alusión a temas técnicos) asegúrese de tener una persona idónea para que pueda dar respuesta a cualquier tipo de solicitud técnica, esta tarea puede ser un poco desgastante los primeros meses y va  a disminuir a medida que el cliente se familiarice con el producto,  tenga presente que si su producto o productos son de bajo costo, en caso de presentar un problema técnico el cliente le exija un cambio de producto o en su defecto la devolución del dinero, esto no seria beneficioso, por ello asegúrese de que sus productos por sencillos que sean cuenten con un alto grado de calidad, aproveche todo el entorno de las redes sociales, chats en vivo y  correos electrónicos, y porqué no utilizar los tickets de soporte técnico.
+              .col-6.col-lg-5
+                figure
+                  img(src='@/assets/curso/temas/t3/t3_04.svg')
+
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-lg-7.mb-4.mb-lg-0.pt-5.pt-0.position-relative
+                .number(data-number="02")
+
+                  p.ps-4.mb-0 Cuente con garantías ante daños en los productos, tenga en cuenta que una garantía casi que es un requerimiento legal, tenga presente que si asegura tener una garantía ampliada será una estrategia adecuada para seguir fidelizando clientes, la garantía consiste en brindarle al cliente una respuesta objetiva de un producto en caso de daño, defectos de fabricación o fallas, siempre explique con claridad qué cubre y no cubre la garantía, durante cuánto tiempo, de esta forma el cliente sabrá a qué está expuesto y no presentará algún tipo de inconformidad referente al producto adquirido.
+
+              .col-6.col-lg-5
+                figure
+                  img(src='@/assets/curso/temas/t3/t3_05.svg')
+
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-lg-7.mb-4.mb-lg-0.pt-5.pt-0.position-relative
+                .number(data-number="03")
+
+                  p.ps-4.mb-0 Permita realizar cambios y devoluciones de los productos, dentro de las ventas online es recurrente que al momento de hacer un cambio el cliente no lo embale adecuadamente y este sufra algún daño al momento de ser transportado y devuelto a su punto de origen, para ello determine un procedimiento ágil y fácil para el comprador, para hacer el cambio de productos o en su defecto devolución del dinero, sea cuidadoso al momento de vender, ya que en un alto porcentaje los clientes no siempre están satisfechos con los productos que compran online, si diseña una buena estrategia su cliente se sentirá a gusto con el producto, con la empresa y seguirá realizando compras, es por ello que se realizan estrategias de postventa encaminadas a la fidelización de clientes.
+
+
+              .col-6.col-lg-5
+                figure
+                  img(src='@/assets/curso/temas/t3/t3_06.svg')
+
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-lg-7.mb-4.mb-lg-0.pt-5.pt-0.position-relative
+                .number(data-number="04")
+
+                  p.ps-4.mb-0 Realice un seguimiento postventa detallado de la compra, cuando un cliente que acaba de pagar un producto se genera un periodo de inseguridad, ya que el cliente podrá sentirse tranquilo al momento de recibir el producto en sus manos, mantenga siempre informado a su cliente de dónde parte el producto, quién lo transporta, dónde se encuentra en un momento determinado y cuál es el periodo de entrega, esto hará que su cliente sienta tranquilidad y credibilidad, hoy en día es muy fácil, ya que la gran mayoría de las empresas especializadas en transportar elementos cuentan con plataformas de tracking, con el código de envío se puede monitorear el pedido en tiempo real.
+
+
+              .col-6.col-lg-5
+                figure
+                  img(src='@/assets/curso/temas/t3/t3_07.svg')
+
+
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-lg-7.mb-4.mb-lg-0.pt-5.pt-0.position-relative
+                .number(data-number="05")
+
+                  p.ps-4.mb-0 Diseñe una encuesta de satisfacción para los clientes (es importante conocer la opinión) y envíela un par de días después de que hayan realizado una compra, donde la opinión y recomendaciones de ellos sean una oportunidad de mejora para la empresa, con el fin de saber cuál es el nivel de satisfacción de su negocio y con el proceso de compra, posterior a esto sistematice y analice qué puede concluir al  momento de recibir el feedback, con ello el cliente se sentirá importante y afianzará su fidelidad con la empresa.
+
+
+              .col-6.col-lg-5
+                figure
+                  img(src='@/assets/curso/temas/t3/t3_08.svg')
+
+
+    p.mb-4 Las estrategias de postventa se enfocan en satisfacer las necesidades del cliente luego de recibir el producto o servicio, de acuerdo con esta información pregúntese cuál sería el compromiso que estaría dispuesto a asumir con un cliente potencial recurrente en este escenario de la postventa.
 </template>
 
 <script>
@@ -26,4 +150,12 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.tarjeta.color-acento-botones--borde
+  background-color: #FDF3E1;
+  color: #12263F;
+  border: solid 4px #EEAC3A;
+
+.slyder-a__tipo-b .slyder-a__btn--atrs, .slyder-a__tipo-b .slyder-a__btn--sigt
+  background-color: #eeac3a;
+</style>
---
+++ src/views/Tema4.vue
@@ -0,0 +1,160 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+       span.text-white 4
+      h1 Niveles de cumplimiento
+
+    .row.justify-content-center.align-items-center
+      .col-lg-10
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/t4/t4_01.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Think about all the possibilities. A good composition is the result of a hierarchy consisting of clearly contrasting elements set with distinct alignments containing irregular intervals of negative space. Nothing of without working at it. Be impossible to ignore.
+              br
+              br
+              | Stand so tall that they can’t look past you. Saul Bass on failure: Failure is built into creativity… the creative act involves this element of ‘newness’ and ‘experimentalism,’ then one must expect accept possibility of failure.
+
+
+    separador
+
+    .titulo-segundo.color-acento-contenido
+      h2#t_4_1 4.1  Medición de entregas
+
+
+    p.mb-4 Una de las maneras con la que se cuenta para garantizar que los servicios y/o productos que llegan al cliente cumplen con la calidad y los requisitos establecidos, es la de realizar un control continuo de cada uno de los procesos que intervienen en la empresa y que hacen posible que el cliente obtenga el producto y/servicios deseados. Un sistema de medición hace énfasis en el conjunto de actividades destinadas a medir, evaluar, ajustar y estar al tanto de todas las actividades de una empresa. El objetivo del sistema de medición es mostrar el camino correcto para que la empresa alcance las metas establecidas. Es importante que un sistema de medición cuente con algunos parámetros para dar óptimos resultados, los cuales podrían ser:
+
+
+    .crd.crd--blue.p-4.mb-5
+      .row.justify-content-center.align-items-center
+        .col-lg-10
+          .row.justify-content-center.align-items-center
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/temas/t4/t4_02.svg", alt="Figura decorativa")
+            .col-lg-7
+              ul.fa-ul.mb-0
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Cuál es el objetivo de medir los tiempos de entrega.
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Identifique cuáles son los problemas y oportunidades de la entrega.
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Delegue quién es el encargado de hacer las entregas.
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Mida el comportamiento a satisfacción del cliente.
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Determine los medios de transporte de entrega.
+                li
+                  i.fa-li.fas.fa-circle.blt_01
+                  | Tenga claro los tiempos de entrega y los canales de comunicación de sus clientes.
+
+    p.mb-5 La esencia de un sistema de medición es entender, orientar y comunicar todas las acciones y estrategias de una empresa, para este caso enfocados en el proceso de entrega de un producto, no requiere tener un sistema base, simplemente cree su sistema personal y adecúelo a la empresa, y pregúntese lo siguiente:
+
+    SlyderF.mb-5(columnas="col-lg-4")(data-aos="fade-in")
+
+      .crd.crd--yellow.bg-crd--yellow.crd--rad.fullHeight.p-4
+        .content
+          p.text-center ¿Cómo va a realizar las entregas de su producto, cuál será el indicador que le brinde una información precisa de los tiempos de entrega y satisfacción de un cliente?
+
+      .crd.crd--blue.bg-crd--blue.crd--rad.fullHeight.p-4
+        .content
+          p.text-center ¿Cómo va a realizar las entregas de su producto, cuál será el indicador que le brinde una información precisa de los tiempos de entrega y satisfacción de un cliente?
+
+      .crd.crd--mint.bg-crd--mint.crd--rad.fullHeight.p-4
+        .content
+          p.text-center Determine cada cuánto realizar un seguimiento o monitoreo de las entregas de los productos, recuerde que cada error es una opción de mejora.
+
+      .crd.crd--yellow.bg-crd--yellow.crd--rad.fullHeight.p-4
+        .content
+          p.text-center ¿Quién es el responsable de hacer el proceso de entrega y seguimiento de los productos?
+
+      .crd.crd--blue.bg-crd--blue.crd--rad.fullHeight.p-4
+        .content
+          p.text-center ¿Qué espera con este tipo de estrategia de medición, cuál es la meta, puede definirlo en cantidad y calidad de entregas, tiempos de entrega o satisfacción del cliente?
+
+      .crd.crd--mint.bg-crd--mint.crd--rad.fullHeight.p-4
+        .content
+          p.text-center Estratégicamente qué hará con los resultados (#[strong esta es la parte más importante del proceso]).
+
+
+      .crd.crd--yellow.bg-crd--yellow.crd--rad.fullHeight.p-4
+        .content
+          p.text-center Registre toda la información pertinente de cada cliente, el pedido, el recorrido y la entrega.
+
+      .crd.crd--blue.bg-crd--blue.crd--rad.fullHeight.p-4
+        .content
+          p.text-center En primer momento diseñe pocos indicadores, la experiencia le dará más opciones para medir lo que desee medir con resultados objetivos.
+
+      .crd.crd--mint.bg-crd--mint.crd--rad.fullHeight.p-4
+        .content
+          p.text-center Posterior a esto, identifique un indicador integral, el cual le arroje la información que necesita para beneficio de su empresa.
+
+    p.mb-4 Un buen sistema de gestión, que incluya medición y control debe estar orientado al futuro, considerar las dimensiones significativas de una actividad con objetivos múltiples. Tenga en cuenta que un mayor control y seguimiento de la gestión no siempre es económicamente deseable.
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass">
+.tarjeta.color-acento-botones--borde
+  background-color: #FDF3E1;
+  color: #12263F;
+  border: solid 4px #EEAC3A;
+
+.slyder-a__tipo-b .slyder-a__btn--atrs, .slyder-a__tipo-b .slyder-a__btn--sigt
+  background-color: #eeac3a;
+
+.slyder-f__btn i
+  color: #eeac3a;
+
+.bg-crd--yellow
+  background-image: url("../assets/curso/temas/t4/cajon_yellow.svg")
+  background-size: cover
+  background-position: left top
+  background-repeat: no-repeat
+  border-radius: 0px !important
+  width: 326px
+  height: 203px
+
+.bg-crd--mint
+  background-image: url("../assets/curso/temas/t4/cajon_green.svg")
+  background-size: cover
+  background-position: left top
+  background-repeat: no-repeat
+  border-radius: 0px !important
+  width: 326px
+  height: 203px
+
+.bg-crd--blue
+  background-image: url("../assets/curso/temas/t4/cajon_blue.svg") !important
+  background-size: cover !important
+  background-position: left top !important
+  background-repeat: no-repeat !important
+  border-radius: 0px !important
+  width: 326px
+  height: 203px
+</style>
